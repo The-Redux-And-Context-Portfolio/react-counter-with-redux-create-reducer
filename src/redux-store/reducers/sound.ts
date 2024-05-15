c11839983917547e7f5aac9ce6bbe4e29011c9c0
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 10:02:58 +0530

    Update sound.ts

--- src/redux-store/reducers/sound.ts
+++ src/redux-store/reducers/sound.ts
@@ -4,7 +4,10 @@ import { createReducer } from "@reduxjs/toolkit";
 /* app imports */
 import { soundOn, soundOff } from "../actions/sound";
 
-/* reducer */
+/**
+  * createReducer - is a redux toolkit method that helps create a reducer function. this is one level (less abstracted) 
+  * than the createSlice method in redux toolkit
+  */
 const initialState = true;
 const soundReducer = createReducer(initialState, (builder) => {
   builder
